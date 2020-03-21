fe22ebce8ee3708da341517b55adcb6f29ccec26
jamie <rmk1075@gmail.com>
2020-03-21 18:05:24 +0900

    google_API geocoding - 경도, 위도

--- crawling/gumi.py
+++ crawling/gumi.py
@@ -17,6 +17,7 @@
 # }
 
 #-*- coding:utf-8 -*-
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -51,15 +52,25 @@ for i in range(0, len(patientsInfo)):
     pInfo = patientsInfo[i].text
     if not pInfo[0].isdigit():
         continue
-    # print(pInfo)
 
     # pInfo 변환 - idx, age, gender, address - 집주소
     tempDict = dict()
     tempDict['idx'] = pInfo.split('번째')[0]
     tempDict['age'] = pInfo.split('(')[1].split('대')[0]
     tempDict['gender'] = pInfo.split('/')[1]
-    tempDict['address'] = pInfo.split('/')[2].strip(')')
+    tempDict['address'] = pInfo.split('/')[2].strip(')').strip()
     patientsInfoDictList.append(tempDict)
+
+    # geocoding
+    url = "https://maps.googleapis.com/maps/api/geocode/xml?address=구미시 " + tempDict['address'] + "&key=AIzaSyAmVnIOsg37a2A31OqbtYrbqadG8y60Dq4"
+    location = BeautifulSoup(requests.get(url).text, "lxml")
+    tempDict['lat'] = location.select("location > lat")[0].get_text()
+    tempDict['lng'] = location.select("location > lng")[0].get_text()
+
+    # TODO:
+    # print(tempDict['address'])
+    # print(location.select("location > lat")[0].get_text())
+    # print(location.select("location > lng")[0].get_text())
 
     pRoute = patientsRoute[i].text.strip().split("\n")
     # for j in range(1, len(pRoute)):
