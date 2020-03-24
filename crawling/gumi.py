b278cb85251e1f010f3f2f813d563efe1cbc02e9
jamie <rmk1075@gmail.com>
2020-03-24 23:31:19 +0900

    patientsRoute js file - 날짜별로 구분 -> 연속된 날짜, 시간별 구분 필요

--- crawling/gumi.py
+++ crawling/gumi.py
@@ -47,7 +47,7 @@ patientsInfo = soup.select('div#sub_cont02 > h4')
 patientsRoute = soup.select('table.tbl_st1 > tbody')
 
 patientsInfoDictList = list()
-# paritentsRouteDict = dict()
+patientsRouteDictList = list()
 for i in range(0, len(patientsInfo)):
     pInfo = patientsInfo[i].text
     if not pInfo[0].isdigit():
@@ -59,7 +59,6 @@ for i in range(0, len(patientsInfo)):
     tempDict['age'] = pInfo.split('(')[1].split('대')[0]
     tempDict['gender'] = pInfo.split('/')[1]
     tempDict['address'] = pInfo.split('/')[2].strip(')').strip()
-    patientsInfoDictList.append(tempDict)
 
     # geocoding
     url = "https://maps.googleapis.com/maps/api/geocode/xml?address=구미시 " + tempDict['address'] + "&key=AIzaSyAmVnIOsg37a2A31OqbtYrbqadG8y60Dq4"
@@ -67,34 +66,42 @@ for i in range(0, len(patientsInfo)):
     tempDict['lat'] = location.select("location > lat")[0].get_text()
     tempDict['lng'] = location.select("location > lng")[0].get_text()
 
-    # pRoute 변환
-    # print(patientsRoute[i])
+    patientsInfoDictList.append(tempDict)
 
+    ######################################################
+
+    # pRoute 변환
     import re
     pattern = '<th .+>.+</th>'
     pRouteDate = re.findall(pattern, (str)(patientsRoute[i]).strip())
     pRoute = re.split(pattern, (str)(patientsRoute[i]).strip())
 
-    print(tempDict['idx'] + '번째 확진자')
-    # print(pRoute)
-
     for j in range(1, len(pRoute)):
-        print(BeautifulSoup(pRouteDate[j-1], 'html.parser').text)
+        tempDict = dict()
+        tempDict['date'] = BeautifulSoup(pRouteDate[j-1], 'html.parser').text
+        # print(BeautifulSoup(pRouteDate[j-1], 'html.parser').text)
+
         if pRoute[j].strip() == "":
             continue
 
-        print(BeautifulSoup(pRoute[j], 'html.parser').text)
-    print('--------------------------')
+        tempDict['Route'] = BeautifulSoup(pRoute[j], 'html.parser').text
+        # print(BeautifulSoup(pRoute[j], 'html.parser').text)
+
+        patientsRouteDictList.append(tempDict)
 
 # print(patientsInfoDictList)
 
 import json
-'''
+
 ## js 파일 - patientsInfo
 f = open("patientsInfo.js", "w", encoding="UTF-8")
 f.write('patientsInfo = ')
 json.dump(patientsInfoDictList, f, ensure_ascii=False)
-'''
+
+## js 파일 - patientsRoute
+f = open("patientsRoute.js", "w", encoding="UTF-8")
+f.write('patientsRoute = ')
+json.dump(patientsRouteDictList, f, ensure_ascii=False)
 
 # 행정구역
 # 선산읍, 고아읍, 무을면, 옥성면, 도개면, 해평면, 산동면, 장천면, 송정동, 원평1동, 원평2동, 지산동, 도량동, 선주원남, 형곡1동, 형곡2동, 신평1동, 신평2동, 비산동, 공단1동, 공단2동, 광평동, 상모사곡, 임오동, 인동동, 진미동, 양포동
