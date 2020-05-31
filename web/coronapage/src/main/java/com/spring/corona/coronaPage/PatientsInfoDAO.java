1c794a085ec6a3fe6dc429321e829cc3e953e0ad
jamie <rmk1075@gmail.com>
2020-05-31 22:56:13 +0900

    coronaPage_spring_jdbc_CRUD

--- web/coronapage/src/main/java/com/spring/corona/coronaPage/PatientsInfoDAO.java
+++ web/coronapage/src/main/java/com/spring/corona/coronaPage/PatientsInfoDAO.java
@@ -2,12 +2,18 @@ package com.spring.corona.coronaPage;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.spring.corona.coronaPage.dto.PatientsInfoDTO;
@@ -15,15 +21,52 @@ import com.spring.corona.coronaPage.dto.PatientsInfoDTO;
 @Repository
 public class PatientsInfoDAO {
 	private NamedParameterJdbcTemplate jdbc; // jdbc template를 사용한다 - template의 이미 작성되어 있는 query를 사용
+	private SimpleJdbcInsert jdbcInsert; // jdbc insert
 	private RowMapper<PatientsInfoDTO> rowMapper = BeanPropertyRowMapper.newInstance(PatientsInfoDTO.class);
+	private String tableName = "patientsInfo";
 	
-	private final String SELECT_ALL = "SELECT * FROM PATIENTSINFO";
+	// sql query
+	private static final String SELECT_ALL = "SELECT * FROM PATIENTSINFO";
+	private static final String UPDATE = "UPDATE PATIENTSINFO SET ADDRESS = :address WHERE IDX = :idx";
+	private static final String SELECT_BY_IDX = "SELECT * FROM PATIENTSINFO WHERE IDX = :idx";
+	private static final String DELETE_BY_IDX = "DELETE FROM PATIENTSINFO WHERE IDX = :idx";
 	
 	public PatientsInfoDAO(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+		this.jdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(tableName);
 	}
 	
 	public List<PatientsInfoDTO> selectAll() {
 		return jdbc.query(SELECT_ALL, Collections.emptyMap(), rowMapper); // sql, bind하는 부분...?, query의 결과를 DTO에 mapping - 저장하기 위한 부분 
+	}
+	
+	public PatientsInfoDTO selectByIdx(Integer idx) {
+		try {
+			Map<String, ?> params = Collections.singletonMap("idx", idx);
+			return jdbc.queryForObject(SELECT_BY_IDX, params, rowMapper);
+		} catch(EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+	
+	// for practice - insert, update, delete
+	public int insert(PatientsInfoDTO patient) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(patient);
+		try {
+			return jdbcInsert.execute(params);				
+		} catch (DuplicateKeyException e) {
+			System.out.println("이미 해당 idx의 데이터가 존재합니다.");
+			return 0;
+		}
+	}
+	
+	public int update(PatientsInfoDTO patient) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(patient);
+		return jdbc.update(UPDATE, params);
+	}
+	
+	public int deleteById(Integer idx) {
+		Map<String, ?> params = Collections.singletonMap("idx", idx);
+		return jdbc.update(DELETE_BY_IDX, params);
 	}
 }
--- web/coronapage/src/main/java/com/spring/corona/coronaPage/exam/JDBCTest.java
+++ web/coronapage/src/main/java/com/spring/corona/coronaPage/exam/JDBCTest.java
@@ -15,11 +15,39 @@ public class JDBCTest {
 		ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 		
 		PatientsInfoDAO dao = ac.getBean(PatientsInfoDAO.class);
+		PatientsInfoDTO patient = new PatientsInfoDTO();
 		
+		// insert
+		patient.setIdx(100);
+		patient.setAge(99);
+		patient.setAddress("서울시");
+		patient.setGender("남성");
+		patient.setLat("123.123");
+		patient.setLng("123.123");
+		int insertCount = dao.insert(patient);
+		System.out.println("alert: " + insertCount + "건이 입력되었습니다.");
+		
+		insertCount = dao.insert(patient);
+		System.out.println("alert: " + insertCount + "건이 입력되었습니다.");
+		
+		// update
+//		patient.setIdx(100);
+//		patient.setAddress("대구시");
+//		int updateCount = dao.update(patient);
+//		System.out.println("alert: " + updateCount + "건이 수정되었습니다.");
+		
+		// searchAll
 		List<PatientsInfoDTO> result = dao.selectAll();
-		for(PatientsInfoDTO patient : result) {
-			System.out.println(patient);
+		for(PatientsInfoDTO p : result) {
+			System.out.println(p);
 		}
+		
+		patient = dao.selectByIdx(100);
+		System.out.println(patient);
+		
+		System.out.println("alert: " + dao.deleteById(100) + "건이 삭제되었습니다.");
+		
+		System.out.println(dao.selectByIdx(100));
 	}
 
 }
